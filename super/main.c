6b93c4308ba43cf0ac1e82a3696d985f69671669
SophiaSilveira-MB <sophia.silveira@montebravo.com.br>
2023-06-01 11:07:56 -0300

    second and third and fouty questions

--- super/main.c
+++ super/main.c
@@ -1,56 +1,83 @@
 #include <stdio.h>
 #include "super.h"
 
-int precoMaior(int compa, float aux) {
-    int i = 0;
+struct produto estoqueFimDia[116] = {};
+
+void gerarEstoque(){
+    for(int i = 0; i < NumProdutos; i++){
+        estoqueFimDia[i] = estoque[i];
+
+        for(int c = 0; c < NumCompras; c++){
+            if(estoqueFimDia[i].codigo == registro[c].codproduto){
+                estoqueFimDia[i].quantidade -= registro[c].quantidade;
+            }
+        }
+        
+        printf("\n%d %s %d %d %g", i + 1, estoqueFimDia[i].nome, estoqueFimDia[i].codigo, estoqueFimDia[i].quantidade, estoqueFimDia[i].precounidade);
+    }
+}
+
+int precoMaior() {
+    float aux = 0;
     int value = 0;
 
-    while(i < NumProdutos ){
-        if(compa == 2){
-            if(estoque[i].precounidade > aux) {
-                aux = estoque[i].precounidade;
-                value = i;}
-        }
-        else{
-            if(estoque[i].precounidade < aux) {
-                aux = estoque[i].precounidade;
-                value = i;}
-        }
-        i++;
+    for(int i = 0; i < NumProdutos; i++){
+        if(estoque[i].precounidade > aux) {
+            aux = estoque[i].precounidade;
+            value = i;}
     }
     return value;
 }
 
-int precoBetween(float valueM, int compa, float aux) {
-    int i = 0;
+int prodMenorI() {
+    int aux = 2000;
     int value = 0;
 
-    while(i < NumProdutos){
-        if(compa == 2){
-            if(estoque[i].precounidade > aux && estoque[i].precounidade < valueM) {
-               aux = estoque[i].precounidade;
-               value = i;}
-        }
-        else{
-            if(estoque[i].precounidade < aux && estoque[i].precounidade > valueM) {
-               aux = estoque[i].precounidade;
-               value = i;}
-        }
-        i++;
+    for(int i = 0; i < NumProdutos; i++){
+        if(estoque[i].quantidade < aux) {
+            aux = estoque[i].quantidade;
+            value = i;}
     }
     return value;
 }
 
-void imprimePreco(char frase[], float value){
-    printf("\n%s %g:\n", frase, value);
-    
-    int i = 0;
+int precoBetween(float valueM) {
+    int value = 0;
+    float aux = 0;
 
-    while(i < NumProdutos){
-        if(estoque[i].precounidade == value){
-            printf("-%s\n", estoque[i].nome);
+    for(int i = 0; i < NumProdutos; i++){
+         if(estoque[i].precounidade > aux && estoque[i].precounidade < valueM) {
+               aux = estoque[i].precounidade;
+               value = i;}
+    }    
+    return value;
+}
+
+int prodBetweenI(int valueM) {
+    int value = 0;
+    float aux = 2000;
+
+    for(int i = 0; i < NumProdutos; i++){
+         if(estoque[i].quantidade < aux && estoque[i].quantidade > valueM) {
+               aux = estoque[i].quantidade;
+               value = i;}
+    }    
+    return value;
+}
+
+void imprime(char frase[], int value, int imp){
+    if(imp == 2) printf("\n%s %g:\n", frase, estoque[value].precounidade);
+    else printf("\n%s %d:\n", frase, estoque[value].quantidade);
+
+    for(int i = 0; i < NumProdutos; i++){
+        if(estoque[i].precounidade == estoque[value].precounidade && imp == 2){
+            printf("-%s codigo %d\n", estoque[i].nome, estoque[i].codigo);
         }
-        i++;
+
+        else if(estoque[i].quantidade == estoque[value].quantidade && imp == 1){
+            printf("-%s codigo %d\n", estoque[i].nome, estoque[i].codigo);
+        }
+
     }
 }
 
@@ -58,28 +85,25 @@ void main(){
 
     printf("Produtos de maior valor por unidade:");
 
-    int primNum = precoMaior(2, 0);
+    int primNum = precoMaior();
+    int secNum = precoBetween(estoque[primNum].precounidade);
+    int thiNum = precoBetween(estoque[secNum].precounidade);
 
-    int secNum = precoBetween(estoque[primNum].precounidade, 2, 0);
+    /*imprime("Primeiro maior valor", primNum, 2);
+    imprime("Segundo maior valor", secNum, 2);
+    imprime("Terceiro maior valor", thiNum, 2);*/
 
-    int thiNum = precoBetween(estoque[secNum].precounidade, 2, 0);
+    printf("\n\nProdutos de menor estoque no inicio do dia:");
 
-    imprimePreco("Primeiro maior valor", estoque[primNum].precounidade);
-    imprimePreco("Segundo maior valor", estoque[secNum].precounidade);
-    imprimePreco("Terceiro maior valor", estoque[thiNum].precounidade);
+    primNum = prodMenorI();
+    secNum = prodBetweenI(estoque[primNum].quantidade);
+    thiNum = prodBetweenI(estoque[secNum].quantidade);
 
-    
-    printf("\n\nProdutos de menor valor por unidade:");
+    /*imprime("Primeira menor quantidade", primNum, 1);
+    imprime("Segunda menor quantidade", secNum, 1);
+    imprime("Terceira menor quantidade", thiNum, 1);*/
 
-    primNum = precoMaior(1, 1000.5);
-
-    secNum = precoBetween(estoque[primNum].precounidade, 1, 1000.5);
-
-    thiNum = precoBetween(estoque[secNum].precounidade, 1, 1000.5);
-
-    imprimePreco("Primeiro menor valor", estoque[primNum].precounidade);
-    imprimePreco("Segundo menor valor", estoque[secNum].precounidade);
-    imprimePreco("Terceiro menor valor", estoque[thiNum].precounidade);
-
+    gerarEstoque();
+ 
 
 }
